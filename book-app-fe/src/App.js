ccd37aefcba0637a953b34f366c5ed901889a3f0
faisal86 <faisalkarim203@gmail.com>
2024-01-18 11:37:12 +0300

    frontend-setup

--- book-app-fe/src/App.js
+++ book-app-fe/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import { useState, useEffect } from 'react';
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import  Axios  from 'axios';
+import {jwtDecode} from "jwt-decode"
 
-function App() {
+import React from 'react'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
