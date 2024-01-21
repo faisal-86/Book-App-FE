8e551f9961aeb174c0223830017657fbf4cdcfa4
ALio <x3liox1@gmail.com>
2024-01-21 12:12:22 +0300

    zz

---
+++ book-app-fe/src/components/layout/SearchBar.jsx
@@ -0,0 +1,25 @@
+// src/SearchBar.js
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    // Pass the query to the parent component (Navbar) for handling
+    onSearch(query);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
