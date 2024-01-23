8861e079cb45b7d7ccac834b3ec10f37e6320a0f
faisal86 <faisalkarim203@gmail.com>
2024-01-23 16:38:11 +0300

    profile

--- book-app-fe/src/App.js
+++ book-app-fe/src/App.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import  Axios  from 'axios';
 import {jwtDecode} from "jwt-decode"
+
 import React from 'react'
 import Signup from './components/registration/SignUp';
 import Signin from './components/registration/SignIn';
@@ -18,6 +19,7 @@ import ProfilePage from './components/registration/Profile';
 import Category from './pages/category/Category'
 import Book from './pages/book/Book';
 import Dropdown from './components/registration/Dropdown';
+import BookDetail from './pages/book/BookDetail';
 
 const passToken =() => { 
   return { headers: { "Authorization": "Bearer " + localStorage.getItem("token")}};
@@ -35,15 +37,21 @@ const [isEdit, setIsEdit] = useState(false);
 const [isEditUser , setIsEditUser] = useState(false);
 
 const fetchUserData = (id) => {
-  Axios.post("user/fetch", { userID: id })
-  .then((response) => {
-    setuserData(response.data.userDetails);
-    setIsAuth(true);
+  Axios.get("/user/detail", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   })
-  .catch((error) => {
-    console.log(error);
-  })
-}
+    .then((response) => {
+      setuserData(response.data.userDetails);
+      setIsAuth(true);
+    })
+    .catch((error) => {
+      console.log(error);
+      // Handle error (e.g., redirect to login page)
+    });
+};
+
 const getUser = () => {
   const token = getToken();
   //need to install jwtDecode (npm i jwt-decode) in the F.E.
@@ -51,16 +59,18 @@ const getUser = () => {
 }
 const getToken = () => {
   const token = localStorage.getItem("token");
+  console.log("TOOOOOKEN",token)
   return token;
 }
 useEffect(() => {
-const user = getUser();
+const guser = getUser();
   //if there is a user then keep everything in check
-  if(user){
+  if(guser != null){
     setIsAuth(true);
-    setUser(true);
+    setUser(guser);
     setUserRole(userRole);
-  fetchUserData(user.id);
+    console.log("the user is",guser)
+  fetchUserData(guser.id);
   } else {
     //else set to false/null and remove token from local storage
     localStorage.removeItem("token");
@@ -97,13 +107,13 @@ const loginHandler = (credentials) => {
     if(token != null){
       //store the token in the browser local storage
       localStorage.setItem("token", token);
-      const user = getUser();
+      const guser = getUser();
       if(user){
         setSignedUp(false);
         navigate('/');
-        fetchUserData(user.id);
-      user ? setIsAuth(true) : setIsAuth(false);
-      user ? setUser(user) : setUser(null);
+        fetchUserData(guser.id);
+      guser ? setIsAuth(true) : setIsAuth(false);
+      guser ? setUser(guser) : setUser(null);
 
       }
     }
@@ -112,7 +122,6 @@ const loginHandler = (credentials) => {
     console.log(error);
     //reset the user and log them out when there is any error with login handling
     setIsAuth(false);
-    setUser(false);
     setUser(null);
   })
 }
@@ -141,7 +150,7 @@ const registerHandler = (user) => {
 
 
 
-
+console.log("MOO",user)
   
   return(
     <>
@@ -204,13 +213,14 @@ const registerHandler = (user) => {
             <Link to="/signup" className="btn btn-warning me-2">Sign Up</Link>
           </div>
           <div className="col-sm">
-      {isAuth ? (
-        <Dropdown/>
-      ) : (
-        <Link to="/signin" className="btn btn-outline-success me-2">
-          <i className="bi bi-box-arrow-in-right"></i>
-        </Link>
-      )}
+          {isAuth ? (
+  <Dropdown user={user} />
+) : (
+  <Link to="/signin" className="btn btn-outline-success me-2">
+    <i className="bi bi-box-arrow-in-right"></i>
+  </Link>
+)}
+
     </div>
         </div>
       </div>
@@ -230,9 +240,10 @@ const registerHandler = (user) => {
           <Route path="/signin" element={<Signin login={loginHandler} />}></Route>
           <Route path="/about" element={<About/>}></Route>
           <Route path='/home' element={<Home/>}> </Route>
-          <Route path='/profile' element={<ProfilePage/>}></Route>
+          <Route path='/profile' element={<ProfilePage user={user}/>}></Route>
           <Route path='/category' element={<Category/>}></Route>
           <Route path='/book' element={<Book/>}></Route>
+          <Route path="/book/:id" element={<BookDetail />} />
         </Routes>
       </main>
     </div>
--- book-app-fe/src/pages/book/BookDetail.jsx
+++ book-app-fe/src/pages/book/BookDetail.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Axios from 'axios';
 
-export default function BookDetail(props) {
+export default function BookDetail() {
+  const { id } = useParams();
+  const [book, setBook] = useState({});
+
+  useEffect(() => {
+    getBook(id);
+  }, [id]);
+
+  function getBook(bookId) {
+    Axios.get(`/book/detail?id=${bookId}`)
+      .then((res) => {
+        setBook(res.data.book);
+      })
+      .catch((err) => {
+        console.log('error getting book data');
+        console.log(err);
+      });
+  }
+
   return (
     <>
-        <td>{props.title}</td>
-        <td>{props.author}</td>
-        <td>{props.description}</td>
-        <td>{props.isbn}</td>
-        <td>{props.publish_date}</td>
-        <td>{props.category}</td>
-        <td>{props.library}</td>
-        <td>{props.review}</td>
-    
-
+      <td>{book.title}</td>
+      <td>{book.author}</td>
+      <td>{book.description}</td>
+      <td>{book.isbn}</td>
+      <td>{book.publish_date}</td>
+      <td>{book.category}</td>
+      <td>{book.library}</td>
+      <td>{book.review}</td>
     </>
-  )
+  );
 }
