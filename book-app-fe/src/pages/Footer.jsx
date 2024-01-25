4d8f9d39af1afe24006f81e0ac2abb8b9752161d
m0hx777 <m0hx@protonmail.com>
2024-01-25 11:47:21 +0300

    changes

--- book-app-fe/src/pages/Footer.jsx
+++ book-app-fe/src/pages/Footer.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Footer() {
   return (
 
-    <footer className="bg-dark text-white py-3" style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+    <footer className="bg-dark text-white py-3" style={{ position: 'relative', bottom: 0, width: '100%' }}>
     <div className="container">
       <div className="row">
         <div className="col-md-6">
